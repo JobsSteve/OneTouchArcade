9dfe6d972a91a2e1d3aad78c674c1b0b7fe61e26
Артур <Artur@MacBook-Pro.local>
2012-03-12 02:29:35 +0300

    Initial Commit

---
+++ GameplayPrototype30/AppDelegate.h
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.h
+//  GameplayPrototype30
+//
+//  Created by Артур on 12.03.12.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class RootViewController;
+
+@interface AppDelegate : NSObject <UIApplicationDelegate> {
+	UIWindow			*window;
+	RootViewController	*viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+
+@end
---
+++ GameplayPrototype30/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  GameplayPrototype30
+//
+//  Created by Артур on 12.03.12.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
